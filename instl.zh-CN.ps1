947a3fa97fb9b7fa83ec1dc40c46cb4953216a7a
Yi <73377514+ilikeyi@users.noreply.github.com>
2021-01-04 21:18:42 +0800

    Update instl.zh-CN.ps1

--- instl.zh-CN.ps1
+++ instl.zh-CN.ps1
@@ -1,10 +1,10 @@
-﻿#
+#
 #    警告：为防止更新后覆盖，请另存为后再修改。
 #
 #    Author: Yi ( https://fengyi.tel )
 #
-#    From: Yi Solution Suite For MSWin Bundled Kit
-#    buildstring: 2.0.0.2.bk_release.201025-1208
+#    From: Yi Solution Suite
+#    buildstring: 5.0.1.0.bk_release.210120-1208
 #
 #
 #    描述:
@@ -20,7 +20,11 @@
 #    先决条件：
 #      - PowerShell 5.1 或 PowerShellCore 7.03 更高
 #
+#    了解或下载：
+#    https://github.com/ilikeyi/powershell.install.software
+#
 
+# 获取脚本参数（如果有）
 [CmdletBinding()]
 param(
     [parameter(Mandatory = $false, HelpMessage = "强行操作.")]
@@ -34,17 +38,17 @@ $app = @(
      [PP]::Wait,
      [FileType]::zip,
      "AIO Repack for Microsoft Visual C++ Redistributable Runtimes",
-     "$env:SystemDrive\Bundled Kit\00\AIO",
+     "$env:SystemDrive\Yi\00\AIO",
      "VisualCppRedist_AIO_x86_x64",
-     "VisualCppRedist_AIO_x86_x64_38",
-     "https://github.com/abbodi1406/vcredist/releases/download/v0.38.0/",
-     "/aiA /gm2"),
+     "VisualCppRedist_AIO_x86_x64_40",
+     "https://github.com/abbodi1406/vcredist/releases/download/v0.40.0/",
+     "/y"),
     ([Status]::Disable,
      [Action]::Install,
      [PP]::Wait,
      [FileType]::exe,
      "Gpg4win",
-     "$env:SystemDrive\Bundled Kit\00\AIO",
+     "$env:SystemDrive\Yi\00\AIO",
      "gpg4win-3.1.13",
      "gpg4win-3.1.13",
      "https://files.gpg4win.org/",
@@ -54,7 +58,7 @@ $app = @(
      [PP]::Wait,
      [FileType]::exe,
      "Python",
-     "$env:SystemDrive\Bundled Kit\00\AIO",
+     "$env:SystemDrive\Yi\00\AIO",
      "python-3.8.5",
      "python-3.8.5",
      "https://www.python.org/ftp/python/3.8.5/",
@@ -64,7 +68,7 @@ $app = @(
      [PP]::Wait,
      [FileType]::exe,
       "Nvidia",
-      "$env:SystemDrive\Bundled Kit\00\Drive",
+      "$env:SystemDrive\Yi\00\Drive",
       "451.67-desktop-win10-64bit-international-dch-whql",
       "451.67-desktop-win10-64bit-international-dch-whql",
       "https://cn.download.nvidia.com/Windows/451.67/",
@@ -74,7 +78,7 @@ $app = @(
      [PP]::Wait,
      [FileType]::exe,
      "酷狗音乐",
-     "$env:SystemDrive\Bundled Kit\00\音乐视频",
+     "$env:SystemDrive\Yi\00\V",
      "kugou9144",
      "kugou9144",
      "http://downmini.kugou.com/web/",
@@ -84,7 +88,7 @@ $app = @(
      [PP]::Wait,
      [FileType]::exe,
      "网易云音乐",
-     "$env:SystemDrive\Bundled Kit\00",
+     "$env:SystemDrive\Yi\00\V",
      "cloudmusicsetup2.7.3.198319",
      "cloudmusicsetup2.7.3.198319",
      "https://d1.music.126.net/dmusic/",
@@ -94,7 +98,7 @@ $app = @(
      [PP]::Fast,
      [FileType]::exe,
      "QQ 音乐",
-     "$env:SystemDrive\Bundled Kit\00\音乐视频",
+     "$env:SystemDrive\Yi\00\V",
      "QQMusicSetup",
      "QQMusicSetup",
      "https://dldir1.qq.com/music/clntupate/",
@@ -104,7 +108,7 @@ $app = @(
      [PP]::Wait,
      [FileType]::exe,
      "腾讯 QQ 2020",
-     "$env:SystemDrive\Bundled Kit\00\聊天工具",
+     "$env:SystemDrive\Yi\00\C",
      "PCQQ2020",
      "PCQQ2020",
      "https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/",
@@ -114,7 +118,7 @@ $app = @(
      [PP]::Wait,
      [FileType]::exe,
      "微信",
-     "$env:SystemDrive\Bundled Kit\00\聊天工具",
+     "$env:SystemDrive\Yi\00\C",
      "WeChatSetup",
      "WeChatSetup",
      "https://dldir1.qq.com/weixin/Windows/",
@@ -149,7 +153,18 @@ Enum FileType
 }
 
 function Get-Version {
-    param($status,$act,$pp,$types,$appname,$folder,$filename,$packer,$url,$param)
+    param(
+        $status,
+        $act,
+        $pp,
+        $types,
+        $appname,
+        $folder,
+        $filename,
+        $packer,
+        $url,
+        $param
+    )
     $url = $url + $packer + "." + $types
     $output = $folder + "\" + $filename+"." + $types
     $outputfoldoer = $folder + "\"
@@ -208,7 +223,7 @@ function Get-Version {
                     {
                         Unzip {
                             if ((Test-Path $output -PathType Leaf)) {
-                                Write-Host "   仅解压..."
+                                Write-Host "    - 仅解压..."
                                 Expand-Archive -LiteralPath $output -DestinationPath $outputfoldoer -force
 
                                 # 解压后删除 ZIP 文件
@@ -218,7 +233,7 @@ function Get-Version {
                         }
                         Install {
                             if ((Test-Path $output -PathType Leaf)) {
-                                Write-Host "   解压后运行..."
+                                Write-Host "    - 解压后运行..."
                                 Expand-Archive -LiteralPath $output -DestinationPath $outputfoldoer -force
 
                                 # 解压后删除 ZIP 文件
@@ -233,12 +248,12 @@ function Get-Version {
                 }
                 else
                 {
-                    Write-Host "   本地存在：$tmpnewpathzip"
+                    Write-Host "    - 本地存在：$tmpnewpathzip"
                     Switch ($act)
                     {
                         Unzip {
                             if ((Test-Path $output -PathType Leaf)) {
-                                Write-Host "   仅解压缩"
+                                Write-Host "    - 仅解压缩"
                                 Expand-Archive -LiteralPath $output -DestinationPath $outputfoldoer -force
 
                                 # 解压后删除 ZIP 文件
@@ -248,7 +263,7 @@ function Get-Version {
                         }
                         Install {
                             if ((Test-Path $output -PathType Leaf)) {
-                                Write-Host "   安装后解压缩。"
+                                Write-Host "    - 安装后解压缩。"
                                 Expand-Archive -LiteralPath $output -DestinationPath $outputfoldoer -force
     
                                 # 解压后删除 ZIP 文件
@@ -301,14 +316,19 @@ function Get-Version {
 }
 
 function Get-RunApp {
-    param($filename,$param,$pp,$sel)
+    param(
+        $filename,
+        $param,
+        $pp,
+        $sel
+    )
 
     if ((Test-Path $filename -PathType Leaf))
     {
         Switch ($pp)
         {
             Fast {
-                Write-Host "   快速执行命令：$filename $param`n"
+                Write-Host "    - 快速执行命令：$filename $param"
                 if ($param -eq ""){
                     switch ($sel)
                     {
@@ -328,7 +348,7 @@ function Get-RunApp {
                 }
             }
             Wait {
-                Write-Host "   执行命令（等待结束）：$filename $param`n"
+                Write-Host "    - 执行命令（等待结束）：$filename $param"
                 if ($param -eq ""){
                     switch ($sel)
                     {
@@ -351,12 +371,14 @@ function Get-RunApp {
     }
     else
     {
-        Write-Host "   未发现安装文件，请检查完整性：$filename。"
+        Write-Host "   未发现安装文件，请检查完整性：$filename"
     }
 }
 
-Write-Host "`n   FYSuite Advanced panel v2.0
-   Author: Yi ( http://fengyi.tel )
+Write-Host "`n   Author: Yi ( http://fengyi.tel )
+
+   From: Yi Solution Suite
+   buildstring: 5.0.1.0.bk_release.210120-1208
 
    安装软件 ( 列表 )
    ------------------------------------"
@@ -371,32 +393,66 @@ for ($i=0; $i -lt $app.Count; $i++) {
         }
     }
 }
-Write-Host "   ------------------------------------`n"
+Write-Host "   ------------------------------------"
+
+function Wait-Exit {
+    param(
+        [int]$wait
+    )
+    Write-Host "`n   提示：$wait 秒后自动退出安装脚本..." -ForegroundColor Red
+    Start-Sleep -s $wait
+    exit
+}
+
+function Install-start {
+    Write-Host "`n   正在安装软件..."
+    Write-Host "   ------------------------------------"
+    for ($i=0; $i -lt $app.Count; $i++) {
+        Get-Version -status $app[$i][0] -act $app[$i][1] -pp $app[$i][2] -types $app[$i][3] -appname $app[$i][4] -folder $app[$i][5] -filename $app[$i][6] -packer $app[$i][7] -url $app[$i][8] -param $app[$i][9]
+    }
+}
+
+function Process-other {
+    Write-Host "`n    处理其它：" -ForegroundColor Green
+
+    Write-Host "    - 删除多余快捷方式"
+    Set-Location "$env:userprofile\Desktop\"
+    Remove-Item -Force -ErrorAction SilentlyContinue ".\Kleopatra.lnk" | Out-Null
+
+    Write-Host "    - 删除开机自启动项"
+    Remove-ItemProperty -ErrorAction SilentlyContinue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Wechat" | Out-Null
+
+    Write-Host "    - 删除计划任务"
+    Disable-ScheduledTask -TaskName GoogleUpdateTaskMachineCore -ErrorAction SilentlyContinue | Out-Null
+    Disable-ScheduledTask -TaskName GoogleUpdateTaskMachineUA -ErrorAction SilentlyContinue | Out-Null
+
+    Write-Host "    - 更名"
+    Set-Location "$env:public\Desktop\"
+    #Rename-Item -Path ".\Google Chrome.lnk" -NewName "谷歌浏览器.lnk" -ErrorAction SilentlyContinue | Out-Null
+}
 
 If ($Force) {
+    Install-start
+    Process-other
 } else {
-	Write-Host "   按任意键开始安装 ...`n"
-	$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    Write-Host "   是否安装以上软件？" -ForegroundColor Green
+
+    $caption="安装软件前请确认。"
+    $message="确认安装（Y），`n取消安装（N）。"
+    $choices = @("&Yes","&No")        
+    $choicedesc = New-Object System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription] 
+    $choices | foreach  { $choicedesc.Add((New-Object "System.Management.Automation.Host.ChoiceDescription" -ArgumentList $_))} 
+    $prompt = $Host.ui.PromptForChoice($caption, $message, $choicedesc, 1)
+    Switch ($prompt)
+    {
+        0 {
+            Install-start
+            Process-other
+            Wait-Exit -wait 6
+        }
+        1 {
+            Write-Host "`n   取消安装。"
+            Wait-Exit -wait 2
+        }
+    }
 }
-
-Write-Host "   正在安装软件..."
-Write-Host "   ------------------------------------"
-for ($i=0; $i -lt $app.Count; $i++) {
-    Get-Version -status $app[$i][0] -act $app[$i][1] -pp $app[$i][2] -types $app[$i][3] -appname $app[$i][4] -folder $app[$i][5] -filename $app[$i][6] -packer $app[$i][7] -url $app[$i][8] -param $app[$i][9]
-}
-Write-Host "   ------------------------------------`n"
-
-Write-Host "   删除多余快捷方式"
-Set-Location "$env:userprofile\Desktop\"
-Remove-Item -Force -ErrorAction SilentlyContinue ".\Kleopatra.lnk" | Out-Null
-
-Write-Host "   删除开机自启动项"
-Remove-ItemProperty -ErrorAction SilentlyContinue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Wechat" | Out-Null
-
-Write-Host "   删除计划任务"
-Disable-ScheduledTask -TaskName GoogleUpdateTaskMachineCore -ErrorAction SilentlyContinue | Out-Null
-Disable-ScheduledTask -TaskName GoogleUpdateTaskMachineUA -ErrorAction SilentlyContinue | Out-Null
-
-Write-Host "   更名"
-Set-Location "$env:public\Desktop\"
-#Rename-Item -Path ".\Google Chrome.lnk" -NewName "谷歌浏览器.lnk" -ErrorAction SilentlyContinue | Out-Null
