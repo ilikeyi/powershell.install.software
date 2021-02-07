0b3de51b1bca0156cd548f64cc98002b8126a99d
Yi <73377514+ilikeyi@users.noreply.github.com>
2021-02-07 13:12:03 +0800

    5.3.1.1

--- en-US/Instl.old.ps1
+++ en-US/Instl.old.ps1
@@ -1,4 +1,5 @@
 ﻿<#
+
   Warning: In order to prevent overwriting after updating, please save as and then modify.
 
   PowerShell installation software
@@ -12,7 +13,8 @@
        - Operating system preferred language: en-US
        - File name: ChromeChrome
        The preferred search condition is GoogleChrome*en-US*, and if the search is not found, search again by default file name.
-    5. Support decompression package processing, etc.
+    5. Support pre-processing, go to function OpenApp {} to change the module;
+    6. Support decompression package processing, etc.
 
   .PREREQUISITES
   - PowerShell 2.0 Or higher
@@ -24,18 +26,34 @@
 
   Package configuration tutorial
 
- Package Configuration        Description
-("Gpg4win",                   Package name
- "Disable",                   Status: Enable - enabled; Disable - disabled
- "Install",                   Action: Install - install; NoInst - does not install after download; Unzip - only extract after download; To - install to directory
- "wait",                      Operation mode: Wait - wait for completion; Fast - run directly
- "auto",                      After setting automatic, the current system disk will be excluded. If no available disk is found, the default setting is the current system disk; specify the drive letter [A:]-[Z:]; specify the path: \\192.168.1.1
- "Installation package\AIO",  Directory Structure
- "https://files.gpg4win.org", Website address
- "gpg4win-latest",            File name downloaded from website
- "exe",                       File type downloaded from the website: exe, zip or custom file type; result: https://files.gpg4win.org/gpg4win-latest.exe
- "gpg4win*",                  File name fuzzy search (*)
- "/S"),                       Operating parameters
+ Package Configuration                                     Description
+("Windows Defender Control",                               Package name
+ "Enable",                                                 Status: Enable - enabled; Disable - disabled
+ "Install",                                                Action: Install - install; NoInst - does not install after download; Unzip - only extract after download; To - install to directory
+ "Wait",                                                   Operation mode: Wait - wait for completion; Fast - run directly
+ "auto",                                                   After setting automatic, the current system disk will be excluded. If no available disk is found, the default setting is the current system disk; specify the drive letter [A:]-[Z:]; specify the path: \\192.168.1.1
+ "Installation package\Tool",                              Directory Structure
+ "https://www.sordum.org/files/download/defender-control", Website address
+ "DefenderControl",                                        File name downloaded from website
+ "zip",                                                    File type downloaded from the website: exe, zip or custom file type; result: https://files.gpg4win.org/gpg4win-latest.exe
+ "DefenderControl*",                                       File name fuzzy search (*)
+ "/D",                                                     Operating parameters
+ "1:DefenderControl:ini")                                  Before running: 1 - select scheme 1; DefenderControl = configuration file name; ini = type, go to function OpenApp {} to change the module
+
+ .Make configuration file
+
+ - default
+   DefenderControl.ini Change to DefenderControl.Default.ini
+
+ - English
+   DefenderControl.ini Change to DefenderControl.en-US.ini
+   Open DefenderControl.en-US.ini and change Language=Auto to Language=English
+
+ - Chinese
+   DefenderControl.ini Change to DefenderControl.zh-CN.ini
+   Open DefenderControl.zh-CN.ini and change Language=Auto to Language=Chinese_简体中文
+
+   Delete DefenderControl.ini after making it.
 
 #>
 
@@ -62,6 +80,7 @@ $app = @(
 	 "Yi",
 	 "deskthemepack",
 	 "Yi*",
+	 "",
 	 ""),
 	("Nvidia GEFORCE GAME READY DRIVER",
 	 "Disable",
@@ -73,7 +92,8 @@ $app = @(
 	 "461.40-desktop-win10-64bit-international-dch-whql",
 	 "exe",
 	 "*-desktop-win10-*-international-dch-whql",
-	 "-s -clean -noreboot -noeula"),
+	 "-s -clean -noreboot -noeula",
+	 ""),
 	("Sysinternals Suite",
 	 "Disable",
 	 "To",
@@ -84,6 +104,7 @@ $app = @(
 	 "SysinternalsSuite",
 	 "zip",
 	 "SysinternalsSuite",
+	 "",
 	 ""),
 	("VisualCppRedist AIO",
 	 "Disable",
@@ -95,7 +116,8 @@ $app = @(
 	 "VisualCppRedist_AIO_x86_x64_43",
 	 "zip",
 	 "VisualCppRedist*",
-	 "/y"),
+	 "/y",
+	 ""),
 	("Gpg4win",
 	 "Disable",
 	 "Install",
@@ -106,7 +128,8 @@ $app = @(
 	 "gpg4win-latest",
 	 "exe",
 	 "gpg4win*",
-	 "/S"),
+	 "/S",
+	 ""),
 	("Python",
 	 "Disable",
 	 "Install",
@@ -117,7 +140,8 @@ $app = @(
 	 "python-3.9.1-amd64",
 	 "exe",
 	 "python-*",
-	 "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0"),
+	 "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0",
+	 ""),
 	("kugou music",
 	 "Disable",
 	 "Install",
@@ -128,7 +152,8 @@ $app = @(
 	 "kugou9175",
 	 "exe",
 	 "kugou*",
-	 "/S"),
+	 "/S",
+	 ""),
 	("NetEase Cloud Music",
 	 "Disable",
 	 "Install",
@@ -139,7 +164,8 @@ $app = @(
 	 "cloudmusicsetup2.7.5.198554",
 	 "exe",
 	 "cloudmusicsetup*",
-	 "/S"),
+	 "/S",
+	 ""),
 	("QQ music",
 	 "Disable",
 	 "Install",
@@ -150,7 +176,8 @@ $app = @(
 	 "QQMusicSetup",
 	 "exe",
 	 "QQMusicSetup",
-	 "/S"),
+	 "/S",
+	 ""),
 	("Tencent QQ 2020",
 	 "Enable",
 	 "Install",
@@ -161,7 +188,8 @@ $app = @(
 	 "PCQQ2021",
 	 "exe",
 	 "PCQQ2021",
-	 "/S"),
+	 "/S",
+	 ""),
 	("WeChat",
 	 "Enable",
 	 "Install",
@@ -172,7 +200,8 @@ $app = @(
 	 "WeChatSetup",
 	 "exe",
 	 "WeChatSetup",
-	 "/S")
+	 "/S",
+	 "")
 )
 # Finally, please don't put , at the end, otherwise you will understand.
 
@@ -239,7 +268,8 @@ function StartInstallSoftware {
 		$packer,
 		$types,
 		$filename,
-		$param
+		$param,
+		$method
 	)
 
 	Switch ($status)
@@ -274,11 +304,9 @@ function StartInstallSoftware {
 					if (TestAvailableDisk -Path $drive)	{
 						$OutTo = Join-Path -Path "$($drive)" -ChildPath "$($structure)"
 						$OutAny = Join-Path -Path "$($drive)" -ChildPath "$($structure)\$($packer).$($types)"
-						$OutArchive = Join-Path -Path "$($drive)" -ChildPath "$($structure)\$($packer).zip"
 					} else {
 						$OutTo = Join-Path -Path $($env:SystemDrive) -ChildPath "$($structure)"
 						$OutAny = Join-Path -Path $($env:SystemDrive) -ChildPath "$($structure)\$($packer).$($types)"
-						$OutArchive = Join-Path -Path $($env:SystemDrive) -ChildPath "$($structure)\$($packer).zip"
 					}
 				}
 			}
@@ -305,55 +333,55 @@ function StartInstallSoftware {
 				Install {
 					Get-ChildItem $OutTo -Recurse -Include "*$($filename)*$((Get-Culture).Name)*.exe" -ErrorAction SilentlyContinue | Foreach-Object {
 						Write-Host "    - Locally exist: $($_.fullname)"
-						OpenApp -filename $($_.fullname) -param $param -mode $mode
+						OpenApp -filename $($_.fullname) -param $param -mode $mode -method $method
 						break
 					}
 					Get-ChildItem $OutTo -Recurse -Include "*$($filename)*.exe" -ErrorAction SilentlyContinue | Foreach-Object {
 						Write-Host "    - Locally exist: $($_.fullname)"
-						OpenApp -filename $($_.fullname) -param $param -mode $mode
+						OpenApp -filename $($_.fullname) -param $param -mode $mode -method $method
 						break
 					}
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Existing installation package"
 					} else {
 						Write-Host "    * Start download`n      > Connected to: $url"
 						try {
-							Write-Host "      + Save to: $OutArchive"
+							Write-Host "      + Save to: $OutAny"
 							CheckCatalog -chkpath $OutTo
-							(New-Object System.Net.WebClient).DownloadFile($url, $OutArchive) | Out-Null
+							(New-Object System.Net.WebClient).DownloadFile($url, $OutAny) | Out-Null
 						} catch {
 							Write-Host "      - Status: Not available`n" -ForegroundColor Red
 							break
 						}
 					}
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Unpacking"
-						Archive -filename $OutArchive -to $OutTo
+						Archive -filename $OutAny -to $OutTo
 						Write-Host "    - Unzip complete"
-						if ((Test-Path $OutArchive)) { remove-item -path $OutArchive -force }
+						if ((Test-Path $OutAny)) { remove-item -path $OutAny -force }
 					} else {
 						Write-Host "    - An error occurred during download`n" -ForegroundColor Red
 					}
 					Get-ChildItem $OutTo -Recurse -Include "*$($filename)*$((Get-Culture).Name)*.exe" -ErrorAction SilentlyContinue | Foreach-Object {
 						Write-Host "    - Locally exist: $($_.fullname)"
-						OpenApp -filename $($_.fullname) -param $param -mode $mode
+						OpenApp -filename $($_.fullname) -param $param -mode $mode -method $method
 						break
 					}
 					Get-ChildItem $OutTo -Recurse -Include "*$($filename)*.exe" -ErrorAction SilentlyContinue | Foreach-Object {
 						Write-Host "    - Locally exist: $($_.fullname)"
-						OpenApp -filename $($_.fullname) -param $param -mode $mode
+						OpenApp -filename $($_.fullname) -param $param -mode $mode -method $method
 						break
 					}
 				}
 				NoInst {
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Installed`n"
 					} else {
 						Write-Host "    * Start download`n      > Connected to: $url"
 						try {
-							Write-Host "      + Save to: $OutArchive"
+							Write-Host "      + Save to: $OutAny"
 							CheckCatalog -chkpath $OutTo
-							(New-Object System.Net.WebClient).DownloadFile($url, $OutArchive) | Out-Null
+							(New-Object System.Net.WebClient).DownloadFile($url, $OutAny) | Out-Null
 						} catch {
 							Write-Host "      - Status: Not available`n" -ForegroundColor Red
 							break
@@ -366,46 +394,46 @@ function StartInstallSoftware {
 						Write-Host "    - Installed`n"
 						break
 					}
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Compressed package available"
 					} else {
 						Write-Host "    * Start download`n        > Connected to: $url"
 						try {
-							Write-Host "      + Save to: $OutArchive"
-							(New-Object System.Net.WebClient).DownloadFile($url, $OutArchive) | Out-Null
+							Write-Host "      + Save to: $OutAny"
+							(New-Object System.Net.WebClient).DownloadFile($url, $OutAny) | Out-Null
 						} catch {
 							Write-Host "      - Status: Not available`n" -ForegroundColor Red
 							break
 						}
 					}
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Unzip only"
-						Archive -filename $OutArchive -to $newoutputfoldoer
+						Archive -filename $OutAny -to $newoutputfoldoer
 						Write-Host "    - Unzip complete`n"
-						if ((Test-Path $OutArchive)) { remove-item -path $OutArchive -force }
+						if ((Test-Path $OutAny)) { remove-item -path $OutAny -force }
 					} else {
 						Write-Host "    - An error occurred during download`n" -ForegroundColor Red
 					}
 				}
 				Unzip {
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Existing installation package"
 					} else {
 						Write-Host "    * Start download      > Connected to: $url"
 						try {
-							Write-Host "      + Save to: $OutArchive"
+							Write-Host "      + Save to: $OutAny"
 							CheckCatalog -chkpath $OutTo
-							(New-Object System.Net.WebClient).DownloadFile($url, $OutArchive) | Out-Null
+							(New-Object System.Net.WebClient).DownloadFile($url, $OutAny) | Out-Null
 						} catch {
 							Write-Host "      - Status: Not available`n" -ForegroundColor Red
 							break
 						}
 					}
-					if (Test-Path -Path $OutArchive) {
+					if (Test-Path -Path $OutAny) {
 						Write-Host "    - Unzip only"
-						Archive -filename $OutArchive -to $OutTo
+						Archive -filename $OutAny -to $OutTo
 						Write-Host "    - Unzip complete`n"
-						if ((Test-Path $OutArchive)) { remove-item -path $OutArchive -force }
+						if ((Test-Path $OutAny)) { remove-item -path $OutAny -force }
 					} else {
 						Write-Host "    - An error occurred during download`n" -ForegroundColor Red
 					}
@@ -414,14 +442,14 @@ function StartInstallSoftware {
 		}
 		default {
 			if ((Test-Path $OutAny -PathType Leaf)) {
-				OpenApp -filename $OutAny -param $param -mode $mode
+				OpenApp -filename $OutAny -param $param -mode $mode -method $method
 			} else {
 				Write-Host "    * Start download`n      > Connected to: $url"
 				try {
 					Write-Host "      + Save to: $OutAny"
 					CheckCatalog -chkpath $OutTo
 					(New-Object System.Net.WebClient).DownloadFile($url, $OutAny) | Out-Null
-					OpenApp -filename $OutAny -param $param -mode $mode
+					OpenApp -filename $OutAny -param $param -mode $mode -method $method
 				} catch {
 					Write-Host "      - Status: Not available`n" -ForegroundColor Red
 					break
@@ -469,8 +497,31 @@ function OpenApp {
 	param(
 		$filename,
 		$param,
-		$mode
+		$mode,
+		$method
 	)
+
+	$Select = $method -split ":"
+	switch ($Select[0]) {
+		1 {
+			$TestCfg = "$(Split-Path $filename)\$($Select[1]).$($Select[2])"
+			$TestDefault = "$(Split-Path $filename)\$($Select[1]).default.$($Select[2])"
+			$TestLanguage = "$(Split-Path $filename)\$($Select[1]).$((Get-Culture).Name).$($Select[2])"
+			if (Test-Path $TestCfg -PathType Leaf){
+				break
+			} else {
+				if (Test-Path $TestLanguage -PathType Leaf){
+					Copy-Item -Path $TestLanguage -Destination $TestCfg -ErrorAction SilentlyContinue
+				} else {
+					if (Test-Path $TestDefault -PathType Leaf){
+						Copy-Item -Path $TestDefault -Destination $TestCfg -ErrorAction SilentlyContinue
+					}
+				}
+			}
+		}
+		default {
+		}
+	}
 
 	if ((Test-Path $filename -PathType Leaf)) {
 		Switch ($mode)
@@ -510,7 +561,7 @@ function ObtainAndInstall {
 	Write-Host "`n   INSTALLING SOFTWARE"
 	Write-Host "   ---------------------------------------------------"
 	for ($i=0; $i -lt $app.Count; $i++) {
-		StartInstallSoftware -appname $app[$i][0] -status $app[$i][1] -act $app[$i][2] -mode $app[$i][3] -todisk $app[$i][4] -structure $app[$i][5] -url $app[$i][6] -packer $app[$i][7] -types $app[$i][8] -filename $app[$i][9] -param $app[$i][10]
+		StartInstallSoftware -appname $app[$i][0] -status $app[$i][1] -act $app[$i][2] -mode $app[$i][3] -todisk $app[$i][4] -structure $app[$i][5] -url $app[$i][6] -packer $app[$i][7] -types $app[$i][8] -filename $app[$i][9] -param $app[$i][10] -method $app[$i][11]
 	}
 }
 
@@ -532,8 +583,8 @@ function Mainpage {
 	Clear-Host
 	Write-Host "`n   Author: Yi ( http://fengyi.tel )
 
-   From: Yi's Solution
-   buildstring: 5.3.1.0.bs_release.210120-1208
+   From: Yi's Solutions
+   buildstring: 5.3.1.1.bs_release.210120-1208
 
    INSTALLED SOFTWARE LIST ( total $($app.Count) items )
    ---------------------------------------------------"
